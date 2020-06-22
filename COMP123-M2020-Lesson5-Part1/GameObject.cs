e3df638f5cc21a079f92b201f04a5244ddbf39e6
Wooram Moon <wpdl555@naver.com>
2020-06-22 11:28:30 -0400

    Commit obejct

---
+++ COMP123-M2020-Lesson5-Part1/GameObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lesson5_Part1
+{
+    class GameObject
+    {
+        // PRIVATE INSTANCE MEMBERS (FIELDS)
+        private string m_name;
+
+        // PUBLIC PROPERTIES
+        public Transform Transform { get; set; }
+
+        public string Name
+        {
+            get 
+            {
+                return m_name;
+            }
+
+            set
+            {
+                m_name = value;
+            }
+        }
+
+        // CONSTRUCTOR(S)
+        public GameObject(string name)
+        {
+            Name = name;
+        }
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHOD
+
+    }
+}
---
+++ COMP123-M2020-Lesson5-Part1/Transform.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lesson5_Part1
+{
+    struct Transform
+    {
+        public Vector2D Position;
+        public Vector2D Rotation;
+        public Vector2D Scale;
+    }
+}
