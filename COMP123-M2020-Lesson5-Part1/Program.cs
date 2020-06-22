896daef1427310e09405413f3d96b8b39543cb17
Wooram Moon <wpdl555@naver.com>
2020-06-22 10:54:08 -0400

    Add project files.

---
+++ COMP123-M2020-Lesson5-Part1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lesson5_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
