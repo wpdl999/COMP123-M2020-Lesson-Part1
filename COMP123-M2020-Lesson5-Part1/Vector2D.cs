9a45fc7d6de15a37e9a07f41b38f2fd3697eb767
Wooram Moon <wpdl555@naver.com>
2020-06-22 11:07:45 -0400

    added Vector23 Struct

---
+++ COMP123-M2020-Lesson5-Part1/Vector2D.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lesson5_Part1
+{
+    struct Vector2D
+    {
+        public float x;
+        public float y;
+    }
+}
